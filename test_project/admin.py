affd22393102612432fca1308fe2e90834f6def9
Javier Riveros <javiiriveros@hotmail.com>
2020-07-09 11:53:47 -0500

    replace admin site with custom admin_site

--- test_project/admin.py
+++ test_project/admin.py
@@ -1,30 +1,25 @@
 # -*- coding: utf-8 -*-
-from django.contrib import admin
-from djangoplicity.contrib.admin.sites import AdminSite
-from djangoplicity.contrib.admin.discover import autoregister
-from django.contrib import admin
-from .models import Author, Article, Book, Entry
-
 # Import all admin interfaces we need
 import django.contrib.sites.admin
+from djangoplicity.contrib.admin.discover import autoregister
+from djangoplicity.contrib.admin.sites import AdminSite
+
 import djangoplicity.customsearch.admin
+from .models import Author, Article, Book, Entry
 
 # Register each applications admin interfaces with
 # an admin site.
 admin_site = AdminSite(name="admin_site")
-adminlogs_site = AdminSite(name="adminlogs_site")
 
 autoregister(admin_site, django.contrib.auth.admin)
 autoregister(admin_site, django.contrib.sites.admin)
 
 autoregister(admin_site, djangoplicity.customsearch.admin)
 
-admin.site.register(Article)
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(Entry)
-
-adminlogs_site.register(django.contrib.sites.models.Site, django.contrib.sites.admin.SiteAdmin)
+admin_site.register(Article)
+admin_site.register(Author)
+admin_site.register(Book)
+admin_site.register(Entry)
 
 admin_site.register(django.contrib.auth.models.User, django.contrib.auth.admin.UserAdmin)
 
