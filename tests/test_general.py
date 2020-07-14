b1241d142e8f4d265e15598a23f96ccb6e5cf0c1
Javier Riveros <javiiriveros@hotmail.com>
2020-07-14 16:39:00 -0500

    move helper methods to utils file

--- tests/test_general.py
+++ tests/test_general.py
@@ -1,60 +1,18 @@
-from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from djangoplicity.customsearch.models import (
-    CustomSearchGroup, CustomSearchModel, CustomSearchField,
-    CustomSearchLayout, CustomSearchLayoutField, CustomSearch,
-    CustomSearchCondition, CustomSearchOrdering, MATCH_TYPE
+    CustomSearchField,
+    CustomSearchLayoutField, CustomSearch,
+    MATCH_TYPE
 )
 from test_project.models import Entry, Author
-
-
-def create_custom_search_model(name, model):
-    return CustomSearchModel.objects.create(name=name, model=ContentType.objects.get_for_model(model))
-
-
-def create_custom_search_field(model, name, selector='', enable_layout=True, enable_search=True, sort_selector=''):
-    return CustomSearchField.objects.create(
-        model=model,
-        name=name,
-        field_name=name,
-        selector=selector,
-        enable_layout=enable_layout,
-        enable_search=enable_search,
-        sort_selector=sort_selector
-    )
-
-
-def create_custom_search_group(name):
-    return CustomSearchGroup.objects.create(name=name)
-
-
-def create_custom_search_condition(search, field, value, match, exclude=False, and_together=False):
-    return CustomSearchCondition.objects.create(
-        search=search,
-        field=field,
-        value=value,
-        match=MATCH_TYPE[match][0],
-        exclude=exclude,
-        and_together=and_together
-    )
-
-
-def create_custom_search_ordering(search, field, descending=False):
-    return CustomSearchOrdering.objects.create(search=search, field=field, descending=descending)
-
-
-def create_custom_search_layout(name, model):
-    return CustomSearchLayout.objects.create(model=model, name=name)
-
-
-def create_custom_search_layout_field(layout, field, position=None):
-    return CustomSearchLayoutField.objects.create(layout=layout, field=field, position=position)
-
-
-def create_custom_search(model, group, layout):
-    return CustomSearch.objects.create(name=model.name, model=model, group=group, layout=layout)
+from .utils import (
+    create_custom_search, create_custom_search_model,
+    create_custom_search_field, create_custom_search_group,
+    create_custom_search_layout_field, create_custom_search_layout, create_custom_search_condition,
+    create_custom_search_ordering
+)
 
 
 class TestModels(TestCase):
